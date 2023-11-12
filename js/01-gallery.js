4e9328b9e690dd9f612d19c2f173d24029cbba8b
NadiaStyslo <sir.ihor@gmail.com>
2023-11-12 12:13:51 -0500

    Update 01-gallery.js
    finished 1th part

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,36 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const container = document.querySelector(".gallery")
-
-const markGallery = createGalleryItemsMarkup(galleryItems);
-console.log(markGallery)
-container.insertAdjacentHTML ("beforeend", markGallery)
-function createGalleryItemsMarkup(items) {
-  return items.map(({ preview, original, description }) =>
-    `<li class="galery__item"> 
-        <a class = "image__link" href = "${original}" 
+//console.log(galleryItems)
+const markGallery = galleryItems.map(({ preview, original, description }) =>
+   `<li class="galery__item"> 
+        <a class = "image__link" href = "${original}" >
     <img class = "gallery__image" src= "${preview}" 
     data-source ="${original}"
     alt = "${description}" />
     </a>
     </li>`).join('');
+container.insertAdjacentHTML('beforeend', markGallery);
+container.addEventListener('click', onClick);
+function onClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains("galery__image")) {
+        
+        return
+        console.log(evt.curentTarget)
+    }
 }
-//console.log(galleryItems);
-
-//const container = document.querySelector(".gallery")
-//console.log(galleryItems)
-//const markGallery = galleryItems.map(({ preview, original, description }) =>
-  //  `<li class = "gallery_item">
-//<a class ="imade_link" href ="${original}"
-//< img class = "image_list" src = "${preview}"
-//data-source ="${original}"
-//alt = "${description}"/>
-//</a></li>`)
-
-//container.insertAdjacentHTML('beforeend', markGallery.join(''));
-//container.addEventListener('click', onClick);
-//function onClick(evt) {
-    
-//}
-
-//console.log(markGallery)
+container.addEventListener('click', onModal)
+function onModal(evt){
+const instance = basicLightbox.create(
+        `<img src = "${evt.target.dataset.source}">`
+    )
+        instance.show()
+        container.addEventListener('keydown', (evt)=>{
+    if( evt.code === "Escape")
+    return instance.close();
+})
+}
